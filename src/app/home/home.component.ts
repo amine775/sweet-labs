801cea28b35faf6f2c0bb78759316bdd61ad4e0d
amine775 <amine_77500@hotmail.fr>
2024-06-29 17:12:41 +0200

    Finish home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,5 +11,34 @@ import { BestSellerComponent } from './best-seller/best-seller.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  showMoreFeature = false;
+
+
+toggleShowMoreFeature() {
+  let extraHeight = 160;
+  this.showMoreFeature = !this.showMoreFeature;
+  if (this.showMoreFeature) {
+    let containers = [document.getElementsByClassName('features-container')[0] as HTMLElement,
+      document.getElementsByClassName('features-info')[0] as HTMLElement,
+      document.getElementsByClassName('features-info-text')[0] as HTMLElement
+    ]
+    for (let container of containers) {
+      let actualContainerHeight = container.clientHeight;
+      let newContainerHeight = actualContainerHeight + extraHeight;
+      container.style.height = `${newContainerHeight}px`;
+    }
+
+  } else {
+    let containers = [document.getElementsByClassName('features-container')[0] as HTMLElement,
+      document.getElementsByClassName('features-info')[0] as HTMLElement,
+      document.getElementsByClassName('features-info-text')[0] as HTMLElement
+    ]
+    for (let container of containers) {
+      let actualContainerHeight = container.clientHeight;
+      let newContainerHeight = actualContainerHeight - extraHeight;
+      container.style.height = `${newContainerHeight}px`;
+    }
+  }
+}
 
 }
