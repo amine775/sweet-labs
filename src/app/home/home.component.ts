d5407ac305322aec5615538ce0f589049b0ced9b
amine775 <amine_77500@hotmail.fr>
2024-08-08 01:34:02 +0200

    refacto design part2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { BestSellerComponent } from './best-seller/best-seller.component';
+import { AboutusComponent } from "./aboutus/aboutus.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, CategoriesComponent, BestSellerComponent],
+  imports: [CarouselComponent, CategoriesComponent, BestSellerComponent, AboutusComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
